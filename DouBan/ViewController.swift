a737acd944a71cc186847247630538f044561db8
Mave <mave@MavedeMacBook-Pro.local>
2014-10-22 16:31:22 +0800

    Initial Commit

---
+++ DouBan/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DouBan
+//
+//  Created by Mave on 14/10/22.
+//  Copyright (c) 2014年 com.gener-tech. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
